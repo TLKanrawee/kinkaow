3422e38da123ef9c258bec2c033c0beb9410ee15
TLKanrawee <kanrawee.pe@ku.th>
2021-10-15 15:19:14 +0700

    Initial commit

---
+++ src/test/java/th/ac/ku/kinkaow/KinkaowApplicationTests.java
@@ -0,0 +1,13 @@
+package th.ac.ku.kinkaow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinkaowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
